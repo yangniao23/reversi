cacf56cc5158678108b37928e5d708f9897355c8
Yang Niao <yangniao23@gmail.com>
2023-12-31 23:43:06 +0900

    add negaalpha wip

--- cpu.c
+++ cpu.c
@@ -142,3 +142,69 @@ int negamax(uint64_t *return_coord, Board *board, Validcoords *validcoords,
     return max_score;
 }
 
+int negaalpha(uint64_t *return_coord, Board *board, Validcoords *validcoords,
+              int16_t prepared_score_matrix[YSIZE][UINT8_MAX], int depth,
+              int alpha, int beta, bool is_pass) {
+    uint64_t coords = validcoords->coords;
+    uint64_t max_coord = 0;
+    int16_t max_score = SHRT_MIN;
+
+    if (depth == 0) {
+        return evaluate(board, prepared_score_matrix);
+    }
+
+    if (coords == 0) {
+        if (is_pass) {
+            return evaluate(board, prepared_score_matrix);
+        } else {
+            Board tmp_board = *board;
+            Validcoords *tmp_validcoords;
+
+            tmp_board.mode = -tmp_board.mode;
+            tmp_validcoords = get_validcoords(&tmp_board);
+            return -negaalpha(NULL, &tmp_board, tmp_validcoords,
+                              prepared_score_matrix, depth, -beta, -alpha,
+                              true);
+        }
+    }
+
+    for (size_t y = 0; y < YSIZE; y++) {
+        for (size_t x = 0; x < XSIZE; x++, coords >>= 1) {
+            if (!(coords & 1)) continue;
+
+            uint64_t put = coord_to_bit(y, x);
+            Board tmp_board = *board;
+            Validcoords *tmp_validcoords;
+            int score;
+            // char coord[3];
+
+            reverse_stones(&tmp_board, validcoords, put);
+
+            tmp_board.mode = -tmp_board.mode;
+            tmp_validcoords = get_validcoords(&tmp_board);
+            /*
+            for (int i = 3 - depth; i > 0; i--) putchar('\t');
+            printf("call negaalpha() (mode=%s, coord=%s, depth=%d)\n",
+                   (tmp_board.mode == BLACK) ? "BLACK" : "WHITE",
+                   bit_to_coord(coord, 3, put), depth - 1);
+            */
+            score = -negaalpha(NULL, &tmp_board, tmp_validcoords,
+                               prepared_score_matrix, depth - 1, -beta, -alpha,
+                               false);
+            /*
+                        for (int i = 3 - depth; i > 0; i--) putchar('\t');
+                        printf("return negaalpha() (coord=%s, score=%d)\n",
+                               bit_to_coord(coord, 3, put), score);
+            */
+            free(tmp_validcoords);
+
+            if (score >= beta) return score;
+            alpha = MAX(alpha, score);
+            max_score = MAX(max_score, score);
+            max_coord = (MAX(max_score, score) == score) ? put : max_coord;
+        }
+    }
+
+    if (return_coord != NULL) *return_coord = max_coord;
+    return max_score;
+}
